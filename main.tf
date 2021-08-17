9dfab80d12e30b480ed3a5d870b4ffc4205e4114
Jag J <jagjandoo@hotmail.ca>
2021-08-17 19:37:11 +0000

    TF update

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
-TEST this
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
